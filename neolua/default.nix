53c2dee167a128ebe1b506fa89495594fee03b5b
Marc Jakobi <marc.jakobi@tiko.energy>
2023-06-04 15:10:40 +0200

    chore: bump neolua version to 1.0.0

--- neolua/default.nix
+++ neolua/default.nix
@@ -1,14 +1,14 @@
 { mkDerivation, base, lib, optparse-applicative, process, text }:
 mkDerivation {
   pname = "neolua";
-  version = "0.1.0";
+  version = "1.0.0";
   src = ./.;
   isLibrary = false;
   isExecutable = true;
   executableHaskellDepends = [
     base optparse-applicative process text
   ];
-  homepage = "https://github.com/mrcjkb/neorocks-nix#readme";
+  homepage = "https://github.com/nvim-neorocks/neorocks#readme";
   description = "A CLI adapter to map Lua's CLI to Neovim's CLI for lua interpretation";
   license = lib.licenses.gpl2Only;
   mainProgram = "neolua";
