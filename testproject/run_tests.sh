ced7e9b26d68487103fea1307121aa2c66f7ab5f
Marc Jakobi <marc.jakobi@tiko.energy>
2023-06-04 16:33:21 +0200

    docs: add testproject/run_tests.sh example script

---
+++ testproject/run_tests.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+# This is an example script on how to run busted tests using Neovim as a lua interpreter locally,
+# without neolua or neorocks.
+
+nvim -c 'lua package.path="lua_modules/share/lua/5.1/?.lua;lua_modules/share/lua/5.1/?/init.lua;"..package.path;package.cpath="lua_modules/lib/lua/5.1/?.so;"..package.cpath;local k,l,_=pcall(require,"luarocks.loader") _=k and l.add_context("busted","2.1.2-3")' -l 'lua_modules/lib/luarocks/rocks-5.1/busted/2.1.2-3/bin/busted' "$@"
